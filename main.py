0d997aa67d022930e1659d3a04f87b37eacc19f1
kingdreamerr <jackmvula458@gmail.com>
2023-02-24 20:43:52 +0200

    Replace the place holder with the person's name

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 with open('./input/Names/invited_names.txt') as names:
     list_of_names = names.readlines()
+
+placeholder = '[name]'
+
+with open('./input/Letters/starting_letter.docx') as letters:
+    letter = letters.read()
+    print(letters)
+    for person in list_of_names:
+        new_name = person.strip('\n')
+        new_letter = letter.replace(placeholder,new_name)
+        # print(new_letter)
